dbd8f533886a559c59e45f8225efe93636d0d9ac
Youri Tjang <mail@tjang.nl>
2016-02-03 16:58:09 +0100

    iets met dingen

--- src/main/java/nl/hr/cmi/inf/Dtos/Hour.java
+++ src/main/java/nl/hr/cmi/inf/Dtos/Hour.java
@@ -10,7 +10,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
-import org.apache.commons.lang.builder.ToStringBuilder;
+//import org.apache.commons.lang.builder.ToStringBuilder;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @Generated("org.jsonschema2pojo")
@@ -113,10 +113,10 @@ public class Hour {
         this.op = op;
     }
 
-    @Override
-    public String toString() {
-        return ToStringBuilder.reflectionToString(this);
-    }
+//    @Override
+//    public String toString() {
+//        return ToStringBuilder.reflectionToString(this);
+//    }
 
     @JsonAnyGetter
     public Map<String, Object> getAdditionalProperties() {
