306b67580d676d3781e0d98b9577af5a76ecab91
marco <marcovannoord@gmail.com>
2016-02-03 16:43:13 +0100

    week toegevoegd

--- src/main/java/nl/hr/cmi/inf/Dtos/Week.java
+++ src/main/java/nl/hr/cmi/inf/Dtos/Week.java
@@ -10,7 +10,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
-import org.apache.commons.lang.builder.ToStringBuilder;
+//import org.apache.commons.lang.builder.ToStringBuilder;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @Generated("org.jsonschema2pojo")
@@ -114,9 +114,9 @@ public class Week {
     }
 
     @Override
-    public String toString() {
-        return ToStringBuilder.reflectionToString(this);
-    }
+ //   public String toString() {
+  //      return ToStringBuilder.reflectionToString(this);
+  //  }
 
     @JsonAnyGetter
     public Map<String, Object> getAdditionalProperties() {
